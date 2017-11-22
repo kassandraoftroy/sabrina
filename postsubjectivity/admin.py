4a9db1fc0900906b82281a0d646d1d7db2cbce45
Sohina Sidhu <Sohina@Sohina-Sidhus-MacBook.local>
2017-11-22 12:41:26 -0500

    fix

--- postsubjectivity/admin.py
+++ postsubjectivity/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Alias, Habla, TextPost
+from models import Alias, Habla, TextPost, ImagePost
 
 admin.site.register(Habla)
 admin.site.register(Alias)
